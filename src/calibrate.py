f41b295fbe399ad0a7d566637acb97cc887e4583
Daniel Joska <user@UCTAdmins-MacBook-Air.local>
2022-06-30 14:02:00 +0200

    Added non-planar calibration algorithm

--- src/calibrate.py
+++ src/calibrate.py
@@ -2,6 +2,15 @@ import cv2 as cv
 import numpy as np
 import os
 import open3d
+import re
+
+def natural_sort(l):
+    """
+    Natural sort function for filenames
+    """
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
 
 def pick_2d_points(image_path):
     """
@@ -14,6 +23,9 @@ def pick_2d_points(image_path):
         if event == cv.EVENT_LBUTTONDOWN:
     
             print([x,y])
+            cv.drawMarker(img, (x, y),(0,0,255), markerType=cv.MARKER_CROSS, 
+                markerSize=40, thickness=2, line_type=cv.LINE_AA)
+            cv.imshow("image", img)
             img_pts.append([x,y])
             
     img = cv.imread(image_path, 1)
@@ -30,7 +42,6 @@ def pick_3d_points(pcd_path):
     """
     Opens a GUI to pick 3d coordinates in a point cloud file
     """
-    import open3d
     a = open3d.io.read_point_cloud(pcd_path)
 
     vis = open3d.visualization.VisualizerWithEditing()
@@ -47,15 +58,15 @@ def pick_3d_points(pcd_path):
     
     return obj_pts
 
-def calibrate_intrisics(img_pts, obj_pts, im_folder, pcd_folder, image_type=".png"):
+def calibrate_intrisics(im_folder, pcd_folder, image_type=".png"):
     """
     Makes use of OpenCV's camera calibration functions to calibrate based on picked points
     """
 
     # get lists of absolute image and pcd filepaths
-    images = [os.path.join(im_folder, im) for im in os.listdir(im_folder) if image_type in im]
+    images = natural_sort([os.path.join(im_folder, im) for im in os.listdir(im_folder) if image_type in im])
     print(images)
-    pcds = [os.path.join(pcd_folder, pcd) for pcd in os.listdir(pcd_folder) if ".pcd" in pcd]
+    pcds = natural_sort([os.path.join(pcd_folder, pcd) for pcd in os.listdir(pcd_folder) if ".pcd" in pcd])
     print(pcds)
 
     all_obj_pts = []
@@ -68,10 +79,38 @@ def calibrate_intrisics(img_pts, obj_pts, im_folder, pcd_folder, image_type=".pn
         obj_mat = pick_3d_points(pcd)
         all_obj_pts.append(obj_mat)
     
+    
+
+    first_im = cv.imread(images[0])
+    first_im_gray = cv.cvtColor(first_im, cv.COLOR_BGR2GRAY)
+
+    all_img_pts_m = np.asarray(all_img_pts, dtype=np.float32)
+    all_obj_pts_m = np.asarray(all_obj_pts, dtype=np.float32)
+
     print("Image Points:")
-    print(all_img_pts)
+    print(all_img_pts_m)
     print("Object Points:")
-    print(all_obj_pts)
+    print(all_obj_pts_m)
+
+    intrinsic_guess = np.asarray([
+        [700, 0, 426],
+        [0, 700, 240],
+        [0, 0, 1]
+    ], dtype=np.float32)
+
+    ret, k, d, r, t = cv.calibrateCamera(all_obj_pts_m, all_img_pts_m, first_im_gray.shape[::-1], intrinsic_guess, None, flags=cv.CALIB_USE_INTRINSIC_GUESS)
+    print("Calibration matrix")
+    print(k)
+    print("Distortion Coeffs")
+    print(d)
+    print("R vecs")
+    print(r)
+    print("t vecs")
+    print(t)
+    if ret:
+        return k, d, r, t
+    else:
+        return 0,0,0,0
 
 
 def get_reprojection_errors(plot=False):
@@ -84,8 +123,10 @@ def get_reprojection_errors(plot=False):
 if __name__=="__main__":
     example_png = "/Users/user/Desktop/wildpose_work/pngs/258.999313280.png"
     example_pcd = "/Users/user/Desktop/wildpose_work/pcds/258.999313280.pcd"
-    pts2d = pick_2d_points(example_png)
-    pts3d = pick_3d_points(example_pcd)
-
-    print(pts2d)
-    print(pts3d)
+    png_folder = "/Users/user/Desktop/pngs"
+    pcd_folder = "/Users/user/Desktop/pcds"
+    #pts2d = pick_2d_points(example_png)
+    #pts3d = pick_3d_points(example_pcd)
+    #print(pts2d)
+    #print(pts3d)
+    calibrate_intrisics(png_folder, pcd_folder)
